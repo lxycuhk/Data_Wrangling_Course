18823e2c86e041b653f7f68be1353ce9acf7da9a
Xinyi <lxycuhk@outlook.com>
2020-05-03 09:18:30 -0400

    filter out records before 2017

--- Final_Project.R
+++ Final_Project.R
@@ -11,5 +11,8 @@ library(choroplethr)
 library(choroplethrMaps)
 
 countries = aq_countries()
+## A test, download latest data in US with the pm25 measurement
 US_latest = aq_latest(country = 'US', parameter = 'pm25')
 glimpse(US_latest)
+
+latest = aq_latest(parameter = 'pm25')
