f0b90507414a52fef9581ab5cc62346de94ed524
Xinyi <lxycuhk@outlook.com>
2020-05-04 11:34:20 -0400

    heatmap

--- Final_Project.R
+++ Final_Project.R
@@ -26,7 +26,7 @@ processed = latest %>% select(location, country, parameter, value, lastUpdated) 
 most = processed %>% group_by(country) %>%
   summarise(n = n()) %>%
   arrange(desc(n)) %>%
-  top_n(n = 20)
+  top_n(n = 20) %>% ungroup()
 
 ## Try plot boxplot with the 10 countries which have most records
 boxplot = processed %>%
@@ -50,6 +50,7 @@ colnames(region) = c('name', 'country')
 combined = as_tibble(left_join(region, avg_sd, by = "country" ))
 mean_table = select(combined, region = name, value = mean)
 mean_table[is.na(mean_table['value']), 2] = 0
+mean_table1 = select(combined, region = name, value = mean)
 
 ## Create the average choropleth map
 country_choropleth(mean_table, num_colors = 9)
@@ -79,7 +80,7 @@ pop['Country'] = pop['Country'] %>%
   tolower()
 colnames(pop)[1] = 'region'
 ## join the pm25 data with population
-value_pop = left_join(mean_table, pop, by = "region") %>% na.exclude()
+value_pop = left_join(mean_table1, pop, by = "region") %>% na.exclude()
 
 ## Download world human development data
 hdi.html = "https://countryeconomy.com/hdi" %>% read_html()
@@ -95,4 +96,35 @@ hdi['Countries'] = hdi['Countries'] %>%
 hdi['HDI'] = -1/log(hdi['HDI'])
 colnames(hdi)[1] = 'region'
 pop_hdi = na.exclude(left_join(value_pop, hdi, by = "region"))
+
+## Label the popualtion and HDI based on the value
+q_pop = quantile(pop_hdi$Population, seq(0.1, 1, 0.1), names = FALSE)
+q_hdi = quantile(c(min(pop_hdi['HDI']),max(pop_hdi['HDI'])), seq(0.1, 1, 0.1), names = FALSE)
+
+pop_hdi_new = pop_hdi %>% 
+  mutate(pop_quantile = case_when(Population <= q_pop[1] ~ '10%',
+                                       Population <= q_pop[2] & Population > q_pop[1] ~ '20%',
+                                       Population <= q_pop[3] & Population > q_pop[2] ~ '30%',
+                                       Population <= q_pop[4] & Population > q_pop[3] ~ '40%',
+                                       Population <= q_pop[5] & Population > q_pop[4] ~ '50%',
+                                       Population <= q_pop[6] & Population > q_pop[5] ~ '60%',
+                                       Population <= q_pop[7] & Population > q_pop[6] ~ '70%',
+                                       Population <= q_pop[8] & Population > q_pop[7] ~ '80%',
+                                       Population <= q_pop[9] & Population > q_pop[8] ~ '90%',
+                                       Population > q_pop[9] ~ '100%')) %>%
+  mutate(hdi_quantile = case_when(HDI <= q_hdi[1] ~ '10%',
+                             HDI <= q_hdi[2] & HDI > q_hdi[1] ~ '20%',
+                             HDI <= q_hdi[3] & HDI > q_hdi[2] ~ '30%',
+                             HDI <= q_hdi[4] & HDI > q_hdi[3] ~ '40%',
+                             HDI <= q_hdi[5] & HDI > q_hdi[4] ~ '50%',
+                             HDI <= q_hdi[6] & HDI > q_hdi[5] ~ '60%',
+                             HDI <= q_hdi[7] & HDI > q_hdi[6] ~ '70%',
+                             HDI <= q_hdi[8] & HDI > q_hdi[7] ~ '80%',
+                             HDI <= q_hdi[9] & HDI > q_hdi[8] ~ '90%',
+                             HDI > q_hdi[9] ~ '100%'))
+
+tile = pop_hdi_new %>% group_by(pop_quantile, hdi_quantile) %>%
+  summarise(mean = log(mean(value)))
+p_cor <-ggplot(tile, aes(pop_quantile, hdi_quantile)) + 
+  geom_tile(aes(fill = mean),colour = "blue") 
   
